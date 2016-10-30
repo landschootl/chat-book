32ddc733ae6d649a4f45428e2489d28c3113d485
Laurent THIEBAULT <thiebault.laurent@gmail.com>
2016-10-30 22:54:47 +0100

    feat(chore): init archi

---
+++ src/main/resources/init.sql
@@ -0,0 +1,38 @@
+/****************** Insertion centres intêrets ******************/
+INSERT INTO HOBBIES VALUES (default, "Sport");
+INSERT INTO HOBBY VALUES (default, 1, "Football");
+INSERT INTO HOBBY VALUES (default, 1, "Rugby");
+INSERT INTO HOBBY VALUES (default, 1, "Natation");
+
+INSERT INTO HOBBIES VALUES (default, "Musique");
+INSERT INTO HOBBY VALUES (default, 2, "Jazz");
+INSERT INTO HOBBY VALUES (default, 2, "Rap");
+INSERT INTO HOBBY VALUES (default, 2, "Classique");
+
+INSERT INTO HOBBIES VALUES (default, "Voyage");
+INSERT INTO HOBBY VALUES (default, 3, "Europe");
+INSERT INTO HOBBY VALUES (default, 3, "Monde");
+INSERT INTO HOBBY VALUES (default, 3, "Allemagne");
+
+INSERT INTO HOBBIES VALUES (default, "Theatre");
+INSERT INTO HOBBY VALUES (default, 4, "Comedie");
+INSERT INTO HOBBY VALUES (default, 4, "Tragedie");
+INSERT INTO HOBBY VALUES (default, 4, "Moliere");
+
+/****************** Insertion compte utilisateur ******************/
+-- compte administrateur
+INSERT INTO USER VALUES (default, "admin", "toto", "titi", "admin", "USER_ADMIN");
+
+-- compte consommateur
+INSERT INTO USER VALUES (default, "TonyLandschoot", "Tony", "Landschoot", "123456", "USER_DEFAULT");
+INSERT INTO USER_HOBBIES VALUES (default, 2, 1);
+INSERT INTO USER_HOBBY VALUES (default, 2, 5);
+
+
+INSERT INTO USER VALUES (default, "SegoleneFrison", "Segolene", "Frison", "123456", "USER_DEFAULT");
+INSERT INTO USER_HOBBIES VALUES (default, 3, 1);
+INSERT INTO USER_HOBBIES VALUES (default, 3, 3);
+
+INSERT INTO USER VALUES (default, "BeatriceDailly", "Beatrice", "Dailly", "123456", "USER_DEFAULT");
+INSERT INTO USER_HOBBY VALUES (default, 4, 2);
+INSERT INTO USER_HOBBY VALUES (default, 4, 9);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,85 @@
+/****************** Configuration de la Base ******************/
+-- creation de la base
+DROP DATABASE IF EXISTS projet_coo;
+CREATE DATABASE projet_coo CHARACTER SET 'utf8';
+USE projet_coo;
+
+/****************** Création des tables ******************/
+-- table des utilisateurs
+DROP TABLE IF EXISTS USER;
+CREATE TABLE USER (
+	id int NOT NULL AUTO_INCREMENT primary key,
+	login varchar(15) NOT NULL UNIQUE,
+	firstname varchar(11) NOT NULL,
+	surname varchar(11) NOT NULL,
+	password varchar(15) NOT NULL,
+	role varchar(12) NOT NULL,
+	constraint u_role_enum check(u_role in ('USER_DEFAULT','USER_ADMIN'))
+);
+
+-- table des groupes de centres d'intêrets
+DROP TABLE IF EXISTS HOBBIES;
+CREATE TABLE HOBBIES (
+	id int NOT NULL AUTO_INCREMENT primary key,
+	name varchar(9) NOT NULL
+);
+
+-- table des centres d'intêrets
+DROP TABLE IF EXISTS HOBBY;
+CREATE TABLE HOBBY (
+	id int NOT NULL AUTO_INCREMENT primary key,
+	id_hobbies int references HOBBIES,
+	name varchar(9) NOT NULL
+);
+
+-- table de relation entre un utilisateur et un groupe de centres d'interets
+DROP TABLE IF EXISTS USER_HOBBIES;
+CREATE TABLE USER_HOBBIES (
+	id int NOT NULL AUTO_INCREMENT primary key,
+	id_user int references USER,
+	id_hobbies int references HOBBIES
+);
+
+-- table de relation entre un utilisateur et un centre d'interet
+DROP TABLE IF EXISTS USER_HOBBY;
+CREATE TABLE USER_HOBBY (
+	id int NOT NULL AUTO_INCREMENT primary key,
+	id_user int references USER,
+	id_hobby int references HOBBY
+);
+
+-- table de relation ntre deux utilisateurs amis
+DROP TABLE IF EXISTS FRIEND;
+CREATE TABLE FRIEND (
+	id int NOT NULL AUTO_INCREMENT primary key,
+	id_user1 int references USER,
+	id_user2 int references USER,
+	confirmed boolean
+);
+
+-- table des groupes de discutions
+DROP TABLE IF EXISTS CONNECTION;
+CREATE TABLE CONNECTION (
+	id int NOT NULL AUTO_INCREMENT primary key,
+	id_admin int references USER,
+	name varchar(9) NOT NULL
+);
+
+-- table de relation entre un utilisateur et un groupe de discution
+DROP TABLE IF EXISTS USER_CONNECTION;
+CREATE TABLE USER_CONNECTION (
+	id int NOT NULL AUTO_INCREMENT primary key,
+	id_connection int references CONNECTION,
+	id_user int references USER
+);
+
+-- table des messages
+DROP TABLE IF EXISTS MESSAGE;
+CREATE TABLE MESSAGE (
+	id int NOT NULL AUTO_INCREMENT primary key,
+	id_connection int references CONNECTION,
+	id_user int references USER,
+	message varchar(100) NOT NULL,
+	prioritaire boolean,
+	expiration date
+);
