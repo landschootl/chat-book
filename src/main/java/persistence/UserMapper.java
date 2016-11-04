ddccc1a76e3e0726bf35cf3f7ede8b387909724f
Laurent THIEBAULT <thiebault.laurent@gmail.com>
2016-11-04 10:25:44 +0100

    gui

--- src/main/java/persistence/UserMapper.java
+++ src/main/java/persistence/UserMapper.java
@@ -3,10 +3,9 @@ package persistence;
 import domain.User;
 import net.rakugakibox.util.YamlResourceBundle;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class UserMapper {
@@ -49,5 +48,27 @@ public class UserMapper {
         rs.close();
 
         return user;
+    }
+
+    public List<User> findAll() throws SQLException {
+        List<User> users = new ArrayList<>();
+        Statement statement;
+        ResultSet rs;
+
+        statement = db.createStatement();
+        rs = statement.executeQuery(this.bundle.getString("select.all.users"));
+
+        while(rs.next()) {
+            users.add(User.builder()
+                    .id(rs.getInt(1))
+                    .login(rs.getString(2))
+                    .firstname(rs.getString(3))
+                    .lastname(rs.getString(4))
+                    .role(rs.getString(5))
+                    .build());
+        }
+        rs.close();
+
+        return users;
     }
 }
