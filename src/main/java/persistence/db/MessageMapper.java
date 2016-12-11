3a6fa338be47ba2a9c0c9ed9c60087f43a731d81
= <ludovic.landschoot@etudiant.univ-lille1.fr>
2016-12-11 23:20:26 +0100

    fix merge

--- src/main/java/persistence/db/MessageMapper.java
+++ src/main/java/persistence/db/MessageMapper.java
@@ -1,5 +1,15 @@
 package persistence.db;
 
+import domain.Discussion;
+import domain.Message;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by landschoot on 11/12/16.
  */
@@ -15,5 +25,51 @@ public class MessageMapper extends Mapper {
             instance = new MessageMapper();
         }
         return instance;
+    }
+
+    private Message createMessage(ResultSet rs) throws SQLException {
+        Message message = Message.builder()
+                .id(rs.getInt("id"))
+                .id_connection(rs.getInt("id_connection"))
+                .message(rs.getString("message"))
+                .accused(rs.getBoolean("accused"))
+                .priority(rs.getBoolean("priority"))
+                .expiration(rs.getDate("expiration") == null ? null : rs.getDate("expiration").toLocalDate())
+                .code(rs.getBoolean("code"))
+                .build();
+        return message;
+    }
+
+    public List<Message> findByDiscussion(Discussion discussion) throws SQLException {
+        List<Message> messages = new ArrayList<>();
+        preparedStatement = db.prepareStatement(this.bundle.getString("select.message.by.discussion"));
+        preparedStatement.setInt(1, discussion.getId());
+        ResultSet rs = preparedStatement.executeQuery();
+        while(rs.next()) {
+            messages.add(createMessage(rs));
+        }
+        rs.close();
+        return messages;
+    }
+
+    public Message create(Message message) {
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = db.prepareStatement(this.bundle.getString("create.message"), PreparedStatement.RETURN_GENERATED_KEYS);
+            preparedStatement.setInt(1, message.getId_connection());
+            preparedStatement.setInt(2, message.getId_user());
+            preparedStatement.setString(3, message.getMessage());
+            preparedStatement.setBoolean(4, message.isAccused());
+            preparedStatement.setBoolean(5, message.isPriority());
+            preparedStatement.setDate(6, message.getExpiration() == null ? null : Date.valueOf(message.getExpiration()));
+            preparedStatement.setBoolean(7, message.isCode());
+            preparedStatement.executeUpdate();
+            ResultSet resultId = preparedStatement.getGeneratedKeys();
+            resultId.next();
+            message.setId(resultId.getInt(1));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return message;
     }
 }
